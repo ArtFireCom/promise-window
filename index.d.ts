8e76ed4769c85074a3da5407da0d6a11e5ebefa8
Jason R. McNeil <jason@jasonrm.net>
2018-04-26 18:35:02 -0700

    Add skeleton TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+interface PromiseWindowOptionsConfig {
+    noopener?: boolean;
+    [key: string]: string | number | boolean;
+}
+
+interface PromiseWindowConfig {
+    width?: number;
+    height?: number;
+    watcherDelay?: number;
+    windowName?: string;
+    window?: PromiseWindowOptionsConfig;
+    onPostMessage?: (event: any) => void;
+    onClose?: () => void;
+    originRegexp?: RegExp;
+    removeOpener: boolean;
+}
+
+declare module "promise-window" {
+    export default class PromiseWindow {
+        public static open(uri: string, config?: PromiseWindowConfig): Promise<Window>;
+
+        public constructor(uri: string, config?: PromiseWindowConfig);
+
+        public open(): Promise<Window>;
+    }
+}
